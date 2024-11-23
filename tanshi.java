366fc34f57f211b43016abd06544e9869cf166d2
tanshiarora77 <aroratanshi3@gmail.com>
2024-11-23 11:17:21 +0530

    Add files via upload

---
+++ tanshi.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+public class tanshi{
+    public static void main(String args[]){
+        int age=18;
+        if (age<=18){
+            System.out.println("younger");
+
+    }
+    else if(age==18){
+        System.out.println("teenager"); 
+}
+else{
+System.out.println("elder");
+}
+    }
+}
