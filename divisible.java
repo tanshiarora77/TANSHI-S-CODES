566f3c28260f5aaee1f5a8fcc14793dcd59d5f8e
tanshiarora77 <aroratanshi3@gmail.com>
2024-11-26 23:07:36 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,17 @@
+
+    import java.util.Scanner;
+ public class divisible{
+       
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the Number :");
+        int number=sc.nextInt();
+        
+        if (number%5==0){
+        System.out.println("It is divisible by 5");
+        }else{
+        System.out.println("It is NOT divisible by 5");
+    } 
+}
+ }
+
